179752eebdf2c268fa6f3f7edb22838d684d3b61
Michael <mschneider247@gmail.com>
2019-11-21 16:06:19 -0700

    Add more GET requests for individual spells and character classes

--- server.js
+++ server.js
@@ -17,6 +17,48 @@ app.get('/api/v1/spells', (request, response) => {
   });
 });
 
+app.get('/api/v1/classes', (request, response) => {
+  database('classes').select()
+    .then((classes) => {
+      response.status(200).json(classes);
+    })
+    .catch((error) => {
+      response.status(500).json({
+        error
+      })
+    });
+});
+
+app.get('/api/v1/spells/:id', (request, response) => {
+  let { id } = request.params;
+  database('spells')
+    .where({ id: id })
+    .then(spell => {
+      if (!spell[0]) {
+        response.status(404).json({ error: `No spell found with id ${id}`})
+      }
+      response.status(200).json(spell[0])
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+});
+
+app.get('/api/v1/classes/:id', (request, response) => {
+  let { id } = request.params;
+  database('classes')
+    .where({ id: id })
+    .then(characterClass => {
+      if (!characterClass[0]) {
+        response.status(404).json({ error: `No character class found with id ${id}`})
+      }
+      response.status(200).json(characterClass[0])
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+});
+
 app.listen(app.get('port'), () => {
   console.log(`App is running on ${app.get('port')}`)
 })
