59b18be2703697729747b13194c23750afd74fd6
Michael <mschneider247@gmail.com>
2019-11-20 21:09:24 -0700

    New spell added

--- spellsData.js
+++ spellsData.js
@@ -8,19 +8,25 @@ let spells = [{
     name: "Acid Arrow",
     level: 2,
     description: "A shimmering green arrow streaks toward a target within range and bursts in a spray of acid.",
-    classes: [1]
+    classes: ['Wizard']
   },
   {
     name: "Arcane Eye",
     level: 4,
     description: "You create an invisible, magical eye within range that hovers in the air for the duration.",
-    classes: [1, 2]
+    classes: ['Wizard', 'Cleric']
   },
   {
     name: "Barkskin",
     level: 1,
     description: "You touch a willing creature. Until the spell ends, the target's skin has a rough, bark-like appearance.",
-    classes: [3, 4]
+    classes: ['Druid', 'Ranger']
+  },
+  {
+    name: "Beacon of Hope",
+    level: 3,
+    description: "This spell bestows hope and vitality",
+    classes: ['Cleric']
   }
 ];
 
