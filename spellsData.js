7abafacb7fa3e6f2b435c1c00c3e105f3d31f8b9
Michael <mschneider247@gmail.com>
2019-11-20 21:22:05 -0700

    Add additional spell blind-or-deaf

--- spellsData.js
+++ spellsData.js
@@ -5,28 +5,34 @@
 
 
 let spells = [{
-    name: "Acid Arrow",
+    name: "Vinegar Arrow",
     level: 2,
     description: "A shimmering green arrow streaks toward a target within range and bursts in a spray of acid.",
     classes: ['Wizard']
   },
   {
-    name: "Arcane Eye",
+    name: "Floating Eye",
     level: 4,
     description: "You create an invisible, magical eye within range that hovers in the air for the duration.",
     classes: ['Wizard', 'Cleric']
   },
   {
-    name: "Barkskin",
+    name: "Treeskin",
     level: 1,
     description: "You touch a willing creature. Until the spell ends, the target's skin has a rough, bark-like appearance.",
     classes: ['Druid', 'Ranger']
   },
   {
-    name: "Beacon of Hope",
+    name: "Ray of Sunshine",
     level: 3,
     description: "This spell bestows hope and vitality",
     classes: ['Cleric']
+  },
+  { 
+    name: "Blind or Deaf?",
+    level: 2,
+    description: "Victim has a 50% chance of going temporarily blind and a seperate 50% chance of going temporarily deaf.",
+    classes: ['Bard', 'Cleric', 'Sorcerer', 'Wizard']
   }
 ];
 
