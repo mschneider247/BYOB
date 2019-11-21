7e952b4f86383419023699d9a58056001fff87ef
Michael <mschneider247@gmail.com>
2019-11-21 10:03:04 -0700

    Add all of the classes data

--- spellsData.js
+++ spellsData.js
@@ -33,6 +33,36 @@ let spells = [{
     level: 2,
     description: "Victim has a 50% chance of going temporarily blind and a seperate 50% chance of going temporarily deaf.",
     classes: ['Bard', 'Cleric', 'Sorcerer', 'Wizard']
+  },
+  {
+    name: "Fire Hands",
+    level: 1,
+    description: "A thin sheet of flames shoot from your outstretched hands to a distance of 15 feet",
+    classes: ['Sorcerer', 'Wizard']
+  },
+  {
+    name: "Touch of the Grave",
+    level: 0,
+    description: "A disembodied skeletal hand appears in mid air and smacks the target",
+    classes: ['Sorcerer', 'Warlock', 'Wizard']
+  },
+  {
+    name: "Mustard Gas",
+    level: 5,
+    description: "Create a 20 foot radius cloud of poisonous gas.",
+    classes: ['Sorcerer', 'Wizard']
+  },
+  {
+    name: "Ass Confetti",
+    level: 1,
+    description: "Fire a short range burst of disorienting glitter",
+    classes: ['Sorcerer', 'Wizard']
+  },
+  {
+    name: "Prayer",
+    level: 5,
+    description: "Ask your diety three yes or no questions",
+    classes: ['Cleric']
   }
 ];
 
